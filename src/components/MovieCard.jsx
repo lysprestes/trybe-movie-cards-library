9d0e9c9aaf4de078609057a06d54b8a5091ce140
Lys <lysprestes@gmail.com>
2021-09-01 17:32:47 -0300

    Updating MpvieCard.jsx and MovieList.jsx
     🛠 Working on 3 to 10 requirements

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,21 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props; // destructuring this.props
+    const { title, subtitle, storyline, rating, imagePath } = movie; // destructuring movie
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <div>
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+          <p>{storyline}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
